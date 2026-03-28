ad04b5727004401b3aed8311757ce025bccaaedf
Caio Carraro Bahlis <caiobahlis@eduroam-226-178-169.laptops.usf.edu>
2026-03-28 13:13:43 -0400

    Backend & SqlAlchemy & Tailwindcss

---
+++ src/backend/app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "postgresql+psycopg://appuser:apppassword@localhost:5432/appdb"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
